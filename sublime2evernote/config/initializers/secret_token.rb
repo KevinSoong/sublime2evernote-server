ad471a774d7bbbb11a91074972ecffc70a9e7e53
Kevin Soong <kjsery@gmail.com>
2013-07-10 17:56:12 -0700

    Genereted rails structure

---
+++ sublime2evernote/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sublime2evernote::Application.config.secret_token = 'bb6c3c741f97b03de02274b56a56841c4325c51ab364d350ce18eb698be506456a8cb7a764ff555ea75568dddea6309801045388ab467011bab7912622a1388a'
